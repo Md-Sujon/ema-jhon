ab48f38848343b78d404a54d7e535e55ed7b0915
Md-Sujon <mdsujon829562@gmail.com>
2022-11-09 13:11:59 +0600

    frist comment

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+           <img src={logo} alt="" />
+           <div>
+            <a href="/shop">Shop</a>
+            <a href="/orders">Orders</a>
+            <a href="/inventory">inventory</a>
+            <a href="/about">About</a>
+            
+           </div>
+        </nav>
+    );
+};
+
+export default Header;
